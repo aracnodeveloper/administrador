40ff8cccb72a90199b42d1667117b59b62ef4f2f
ecarrillo99 <70332717+aracnodeveloper@users.noreply.github.com>
2026-04-01 09:46:33 -0500

    auth

--- src/services/service.jsx
+++ src/services/service.jsx
@@ -17,6 +17,7 @@ class GenericService {
 
     async post(url, params) {
       var headers={
+        Autorization: "bearer " + Config.DEVELOPER_TOKEN,
         'Content-Type': 'application/json; charset=utf-8'
       }
       
